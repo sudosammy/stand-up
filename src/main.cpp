695ffbe52e08783dc38fcd184cf2e84a823ba066
sudosammy <sudosammy@users.noreply.github.com>
2023-09-05 23:37:00 +0800

    new TFT library + RTC Clock function

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,49 @@
 #include <Arduino.h>
 #include <NewPing.h>
 #include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <TFT_ILI9163C.h>
 #include <RTClib.h>
-#include "settings.h" // config
+#include <ssd1306.h>
+#include <nano_engine.h>
+//#include "sova.h"
 
+#include "settings.h" // config
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // Setup sensor
-TFT_ILI9163C tft = TFT_ILI9163C(TFT_CS, TFT_DC, TFT_RST); // Setup TFT
 RTC_DS3231 rtc; // Setup clock
 
-unsigned long testText(int dist) {
-  tft.fillScreen();
+bool textDemo(int dist) {
+    ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_clearScreen8();
+    ssd1306_setColor(RGB_COLOR8(255,255,0));
+    ssd1306_printFixed8(8,  16, "put dist in here", STYLE_NORMAL);
 
-  tft.setCursor(10, 40);
-  tft.setTextColor(WHITE);  
-  tft.setTextSize(3);
-  tft.println(String(dist) + " cm");
-  return true;
+    return true;
 }
 
 void setup() {
   Serial.begin(9600);
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (1) delay(10);
+  if (CLOCK_CONNECTED) {
+    if (! rtc.begin()) {
+      Serial.println("Couldn't find RTC");
+      Serial.flush();
+      while (1) delay(10);
+    }
+
+    if (rtc.lostPower()) {
+      Serial.println("RTC lost power, let's set the time!");
+      // When time needs to be set on a new device, or after a power loss, the
+      // following line sets the RTC to the date & time this sketch was compiled
+      rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
   }
 
-  tft.begin();
-  tft.setRotation(2); // if screen if upside down
-
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
+  ssd1306_setFixedFont(ssd1306xled_font6x8);
+  il9163_setOffset(2, 1);
+  il9163_128x128_spi_init(TFT_RST, TFT_CS, TFT_DC);
+  ssd1306_setMode(LCD_MODE_NORMAL);
+  ssd1306_clearScreen8();
 }
 
 void loop() {
@@ -51,22 +55,26 @@ void loop() {
   Serial.print(distance);
   Serial.println("cm");
 
-  testText(distance);
+  //il9163_setRotation(0); // this seems to shift the offset -1px on the x-axis. wtf?
+  textDemo(distance);
 
-  DateTime now = rtc.now();
-    
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+  if (CLOCK_CONNECTED) {
+    DateTime now = rtc.now();
+      
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+  }
+  
 }
--- src/settings.h
+++ src/settings.h
@@ -1,14 +1,10 @@
 // Senor pins
-#define TRIGGER_PIN  6
-#define ECHO_PIN     5
-#define MAX_DISTANCE 300 // Maximum distance we want to measure (in centimeters).
+#define TRIGGER_PIN     6
+#define ECHO_PIN        5
+#define MAX_DISTANCE    300 // Maximum distance we want to measure (in centimeters).
 
-// Specific to my TFT
-#define _TFTWIDTH  		130
-#define _TFTHEIGHT 		130
-#define _GRAMWIDTH      130
-#define _GRAMHEIGH      130
-#define __OFFSET		1
+// Clock pins
+#define CLOCK_CONNECTED false // set false if no clock
 
 // TFT details
 // LED = 5v (contains 100 ohm resistor)
@@ -19,16 +15,6 @@
 // CS = SS 10
 // GND = GND
 // VCC = 3.3v
-#define TFT_CS   10
-#define TFT_DC   9
-#define TFT_RST  8
-
-// Colour definitions
-#define	BLACK   0x0000
-#define	BLUE    0x001F
-#define	RED     0xF800
-#define	GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0  
-#define WHITE   0xFFFF
+#define TFT_CS          10
+#define TFT_DC          9
+#define TFT_RST         8
