9626abb92aa8d3183635f80cebdc9857e1346569
sudosammy <sudosammy@users.noreply.github.com>
2023-09-05 20:56:37 +0800

    rtc

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <TFT_ILI9163C.h>
+#include <RTClib.h>
 #include "settings.h" // config
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // Setup sensor
 TFT_ILI9163C tft = TFT_ILI9163C(TFT_CS, TFT_DC, TFT_RST); // Setup TFT
+RTC_DS3231 rtc; // Setup clock
 
 unsigned long testText(int dist) {
   tft.fillScreen();
@@ -21,8 +25,21 @@ unsigned long testText(int dist) {
 void setup() {
   Serial.begin(9600);
 
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
   tft.begin();
   tft.setRotation(2); // if screen if upside down
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop() {
@@ -35,4 +52,21 @@ void loop() {
   Serial.println("cm");
 
   testText(distance);
+
+  DateTime now = rtc.now();
+    
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
 }
